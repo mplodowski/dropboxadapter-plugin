eb2a3b94e887999d4397a1534cb8eb519417a550
Michał Płodowski <michal.plodowski@gmail.com>
2022-06-16 21:36:30 +0200

    Typo

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,6 +3,6 @@
 return [
     'plugin' => [
         'name' => 'Dropbox Adapter Plugin',
-        'description' => 'Add Dropbox filesystem adapter to October CMS.',
+        'description' => 'Add Dropbox filesystem adapter for October CMS.',
     ],
 ];
